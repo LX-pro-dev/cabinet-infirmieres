3b94df69a61f465be21ec7311d402ed1754c4704
@lx.pro.dev <lx.pro.dev@gmail.com>
2021-10-01 10:15:27 +0200

    fix: CRUD finished

--- src/main/java/com/example/cabinetinfirmieresback/repositories/PatientRepository.java
+++ src/main/java/com/example/cabinetinfirmieresback/repositories/PatientRepository.java
@@ -1,2 +1,7 @@
-package com.example.cabinetinfirmieresback.repositories;public class PatientRepository {
+package com.example.cabinetinfirmieresback.repositories;
+
+import com.example.cabinetinfirmieresback.models.Patient;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PatientRepository extends JpaRepository<Patient, Integer> {
 }
--- src/main/java/com/example/cabinetinfirmieresback/services/impl/PatientServiceImpl.java
+++ src/main/java/com/example/cabinetinfirmieresback/services/impl/PatientServiceImpl.java
@@ -1,2 +1,31 @@
-package com.example.cabinetinfirmieresback.services.impl;public class PatientServiceImpl {
+package com.example.cabinetinfirmieresback.services.impl;
+
+import com.example.cabinetinfirmieresback.models.Patient;
+import com.example.cabinetinfirmieresback.repositories.PatientRepository;
+import com.example.cabinetinfirmieresback.services.PatientService;
+
+import java.util.List;
+
+public class PatientServiceImpl implements PatientService {
+
+    private PatientRepository patientRepository;
+
+    public PatientServiceImpl(PatientRepository patientRepository) {
+        this.patientRepository = patientRepository;
+    }
+
+    @Override
+    public List<Patient> findAll() { return this.patientRepository.findAll(); }
+
+    @Override
+    public Patient findById(Integer id) { return this.patientRepository.findById(id).orElse(null);  }
+
+    @Override
+    public Patient postPatient(Patient patient) { return this.patientRepository.save(patient); }
+
+    @Override
+    public Patient putPatient(Patient patient) { return this.patientRepository.save(patient); }
+
+    @Override
+    public void deletePatient(Integer id) { this.patientRepository.deleteById(id); }
 }
